49325aa62c011b0e87f87c3569bd8856f02f66fe
schyflakes11-a11y <schyflakes11@gmail.com>
2026-05-11 20:25:51 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,7 +14,9 @@ class Client(discord.Client):
         if message.author == self.user:
             return
         if message.content.startswith("$emi"):
-            bot_response = await emi(f"{message.author}: {message.content}")
+            string = message.author + ": " + message.content
+            string2 = string[4:]
+            bot_response = await emi(string2)
             messages.append({"role": "assistant", "content": bot_response})
             await message.channel.send(bot_response)
 async def emi(prompt):
